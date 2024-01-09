4fbfdaedc21ecea4c38bf7b2d0639f76b3bc526f
Sadman600 <sadmansakib600@gmail.com>
2024-01-09 23:30:07 +0600

    added unpkt react, dom , and bable

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-console.log("Hello Frontend");
-console.log("ReactDOM", ReactDOM);
+// const App = React.createElement("p", { style: { color: "red" } }, "Hello");
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello App</h1>
+        </>
+    )
+
+}
+// const container = document.getElementById("root");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
