61d8a0aed2636acc637fe5c0c7f7549ff2544e0a
Sadman600 <sadmansakib600@gmail.com>
2024-01-10 12:25:19 +0600

    replace plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,10 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-
+import commonjs from '@rollup/plugin-commonjs';
 export default {
 	input: 'src/main.js',
 	output: {
 		file: 'public/bundle.js',
 		format: 'esm'
 	},
-	plugins: [nodeResolve({ extensions: ['js', 'jsx'] })]
+	plugins: [nodeResolve({ extensions: ['js', 'jsx'] }), commonjs()]
 };
