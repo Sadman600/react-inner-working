ef79c2720c459a1a9ba27bc1d39783804ac805f1
Sadman600 <sadmansakib600@gmail.com>
2024-01-10 10:31:53 +0600

    added react dom

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-// const App = React.createElement("p", { style: { color: "red" } }, "Hello");
+const App = React.createElement("p", { style: { color: "red" } }, "Hello");
 
-const App = () => {
-    return (
-        <>
-            <h1>Hello App</h1>
-        </>
-    )
+// const App = () => {
+//     return (
+//         <>
+//             <h1>Hello App</h1>
+//         </>
+//     )
 
-}
+// }
 // const container = document.getElementById("root");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+// root.render(<App />);
+root.render(React.createElement(App));
